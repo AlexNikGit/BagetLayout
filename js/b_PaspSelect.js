101ddf410d1f0556ef7750918bff5331dff06009
AlexNikGit <alexnikmail@gmail.com>
2015-08-03 23:51:03 +0300

    разработка структуры страницы

--- js/b_PaspSelect.js
+++ js/b_PaspSelect.js
@@ -40,23 +40,44 @@ function genPaspSheets( ) {
 
       var container = document.getElementById( "b_PaspSheets" );
 
-      var arr = json_array.pasp_list;
+      var arr = json_array.pasp_sheets;
+      var sps = 1;    /* selected paspartu sheet */
       var out = '';
-      out += '<ul id="listPaspartu">';
       var i;
+      out += '<section class="dropdown">';
+      out +=    '<label>Слои багета</label>';
+      out +=    '<div>';
+      out +=       '<ul>';
       for( i = 0; i < arr.length; i++ ) {
-         out += '<li>' +
-            '<img style="background-color: ' + arr[ i ].color + ';"/>' +
-            '<span>' + arr[ i ].code + '</span>' +
-            '<span>' + arr[ i ].c_text + '</span>' +
-            '</li>';
+         out +=       '<li><a href="index.html">' + arr[ i ].sheet_num + '-й слой</a></li>';
       }
-      out += '</ul>';
-
-      //container.innerHTML = out;
+      out +=       '</ul>';
+      out +=    '</div>';
+      out += '</section>';
+      out += '<section class="lc_PaspSheetInfo gc_ThinBorder">';
+      out +=    '<h5>' + arr[ sps ].sheet_num + '-й слой [' + arr[ sps ].pasp_code + ']<button>...</button></h5>';
+      out +=    '<div>';
+      /* добавить возможность выбора разной ширины для разных (верх, право, низ, лево) полей */
+      out +=       '<span>Ширина поля</span>';
+      out +=       '<input type="text" name="SheetWidth" id="txtPaspSheetWidth" value="' + arr[ sps ].pasp_width + '">';
+      out +=    '</div>';
+      out +=    '<div>';
+      out +=       '<span>Цвет/текстура</span>';
+      out +=       '<img style="background-color: ' + arr[ sps ].color + ';"/>';
+      out +=    '</div>';
+      out += '</section>';
+      out += '<section class="lc_PaspSheetManip  gc_ThinBorder">';
+      out +=    '<div>';
+      /* добавить обработчик oncheck (при выборе появляется выбор декора для паспарту в поле "кант/декор") */
+      out +=       '<input type="checkbox">';
+      out +=       '<span>Добавить декор</span>';
+      out +=    '</div>';
+      out +=    '<button>Добавить слой</button>';
+      out += '</section>';
+      container.innerHTML = out;
    };
    var xhr = new XMLHttpRequest();
-   xhr.open( 'GET', 'json/pasp_list.json' );
+   xhr.open( 'GET', 'json/pasp_sheets.json' );
    xhr.responseType = 'json';
    xhr.withCredentials = true;
    xhr.onload = onLoadHandler;
--- js/b_ProductOptions.js
+++ js/b_ProductOptions.js
@@ -8,7 +8,7 @@ function loadDecorationWidget( ) {
    var onLoadHandler = function( event ) {
       var doc = event.target.response;
 
-      var container = document.getElementById( "tab_c1" );        // заменить на поиск внутри b_ProductOptions
+      var container = document.getElementById( "tab_c1" );
       container.innerHTML = '';       // очистка контейнера
       var wdg_element = doc.getElementById( "w_OptDecoration" );
       container.appendChild( wdg_element );
@@ -36,24 +36,52 @@ function loadPickingWidget( ) {
    var onLoadHandler = function( event ) {
       var doc = event.target.response;
 
-      var container = document.getElementById( "tab_c2" );        // заменить на поиск внутри b_ProductOptions
+      var container = document.getElementById( "tab_c2" );
       container.innerHTML = '';       // очистка контейнера
       var wdg_element = doc.getElementById( "w_PickingOptions" );
       container.appendChild( wdg_element );
 
       /* загружаем скрипты и стили для обработки загружаемого блока */
       /*var js_ref=document.createElement('script');
-      js_ref.setAttribute("src", "js/b_ProductOptions.js");
+      js_ref.setAttribute("src", "js/w_PickingOptions.js");
       var css_ref=document.createElement("link");
       css_ref.setAttribute("rel", "stylesheet");
       css_ref.setAttribute("type", "text/css");
-      css_ref.setAttribute("href", "css/b_ProductOptions.css");
+      css_ref.setAttribute("href", "css/w_PickingOptions.css");
       var doc_header = document.getElementsByTagName( "head" )[0];
       doc_header.appendChild( js_ref );
       doc_header.appendChild( css_ref );*/
    };
    var xhr = new XMLHttpRequest();
    xhr.open( 'GET', 'pages/widgets/w_OptPicking.html' );
+   xhr.responseType = "document";
+   xhr.withCredentials = true;
+   xhr.onload = onLoadHandler;
+   xhr.send( );
+}
+
+function loadMaterialWidget( ) {
+   var onLoadHandler = function( event ) {
+      var doc = event.target.response;
+
+      var container = document.getElementById( "tab_c3" );
+      container.innerHTML = '';       // очистка контейнера
+      var wdg_element = doc.getElementById( "w_MaterialUsage" );
+      container.appendChild( wdg_element );
+
+      /* загружаем скрипты и стили для обработки загружаемого блока */
+      /*var js_ref=document.createElement('script');
+       js_ref.setAttribute("src", "js/w_MaterialUsage.js");
+       var css_ref=document.createElement("link");
+       css_ref.setAttribute("rel", "stylesheet");
+       css_ref.setAttribute("type", "text/css");
+       css_ref.setAttribute("href", "css/w_MaterialUsage.css");
+       var doc_header = document.getElementsByTagName( "head" )[0];
+       doc_header.appendChild( js_ref );
+       doc_header.appendChild( css_ref );*/
+   };
+   var xhr = new XMLHttpRequest();
+   xhr.open( 'GET', 'pages/widgets/w_MaterialUsage.html' );
    xhr.responseType = "document";
    xhr.withCredentials = true;
    xhr.onload = onLoadHandler;
